c7a97006c656910faa4b33eb3197182eac09ff78
kavidumdbot <herokobot4@gmail.com>
2025-08-05 23:21:47 +0530

    Update check-uptime.js

--- plugins/check-uptime.js
+++ plugins/check-uptime.js
@@ -119,8 +119,8 @@ ${config.DESCRIPTION}`;
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363388320701164@newsletter',
-                    newsletterName: config.OWNER_NAME || 'JesterTechX',
+                    newsletterJid: '120363422383085384@newsletter',
+                    newsletterName: config.OWNER_NAME || 'kavidu induwara',
                     serverMessageId: 143
                 }
             }
