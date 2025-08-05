b78f33c03bd42c7c5e6ed2a484475f9403187c10
kavidumdbot <herokobot4@gmail.com>
2025-08-05 23:23:28 +0530

    Update check-weather.js

--- plugins/check-weather.js
+++ plugins/check-weather.js
@@ -28,7 +28,7 @@ async (conn, mek, m, { from, q, reply }) => {
 > 💨 *Wind Speed*: ${data.wind.speed} m/s
 > 🔽 *Pressure*: ${data.main.pressure} hPa
 
-> *© Powdered By JesterTechX*
+> *© Powdered By kavidu induwara*
 `;
         return reply(weather);
     } catch (e) {
