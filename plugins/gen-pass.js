0a66d594d95a2753fa0127a9c0a4ff58dd6e49b7
kavidumdbot <herokobot4@gmail.com>
2025-08-05 23:43:03 +0530

    Update gen-pass.js

--- plugins/gen-pass.js
+++ plugins/gen-pass.js
@@ -56,7 +56,7 @@ cmd({
 
     // Send the message with the generated password
     await conn.sendMessage(from, {
-      text: "🔐 *Your Strong Password* 🔐\n\nPlease find your generated password below:\n\n" + generatedPassword + "\n\n*Powered By JesterTechX*"
+      text: "🔐 *Your Strong Password* 🔐\n\nPlease find your generated password below:\n\n" + generatedPassword + "\n\n*Powered By 𝙺𝙰𝚅𝙸𝙳𝚄-𝙼𝙳*"
     }, {
       quoted: quoted
     });
