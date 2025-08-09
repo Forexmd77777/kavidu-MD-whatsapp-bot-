4ca082ae68cd3cafe3ebd1c3e067c7e26bc77284
kavidumdbot <herokobot4@gmail.com>
2025-08-09 20:04:49 +0530

    Update repo-search.js

--- plugins/repo-search.js
+++ plugins/repo-search.js
@@ -25,7 +25,7 @@ cmd({
     responseMsg += `🍴 *Forks*: ${data.forks_count}\n`;
     responseMsg += `👤 *Owner*: ${data.owner.login}\n`;
     responseMsg += `📅 *Created At*: ${new Date(data.created_at).toLocaleDateString()}\n`;
-    responseMsg += `\n> *© Powered by JesterTechX*`;
+    responseMsg += `\n> *© Powered by 𝙺𝙰𝚅𝙸𝙳𝚄-𝙼𝙳;
 
     await conn.sendMessage(from, { text: responseMsg }, { quoted: m });
   } catch (error) {
